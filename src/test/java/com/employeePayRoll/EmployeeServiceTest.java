778481cb650d9182ea08669baa7394d9e1bd341f
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 04:38:53 +0530

    Added:Test case to check for reading of employee from a file

--- src/test/java/com/employeePayRoll/EmployeeServiceTest.java
+++ src/test/java/com/employeePayRoll/EmployeeServiceTest.java
@@ -35,4 +35,18 @@ class EmployeeServiceTest {
 		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
 		assertEquals(3, result);
 	}
+	/**
+	 * Usecase7 
+	 * Test case to check for reading the data from file and adding the data to list
+	 */
+	@Test
+	public void given3Employee_WhenWrittenToFile_ShouldBeReadFromFile() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		eService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
 }
