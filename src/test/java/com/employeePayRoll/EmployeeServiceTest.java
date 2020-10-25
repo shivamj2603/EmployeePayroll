f22d7ad662ca302434b27e3811cd76483c210628
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 04:13:51 +0530

    Added:Functionality to test whether data is written to file or not

---
+++ src/test/java/com/employeePayRoll/EmployeeServiceTest.java
@@ -0,0 +1,16 @@
+package com.employeePayRoll;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class EmployeeServiceTest {
+	@Test
+	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
+}
