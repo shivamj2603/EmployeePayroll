2fd78030cb9e3d922bb1820c5c6a36bd2e341a59
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 03:47:29 +0530

    Added:Functionality to read the employee data from console and write to console

--- src/main/java/com/employeePayRoll/Employee.java
+++ src/main/java/com/employeePayRoll/Employee.java
@@ -1,5 +1,19 @@
 package com.employeePayRoll;
 
 public class Employee {
-
+	public String name;
+	public int id;
+	public double salary;
+	public Employee(int id, String name, double salary) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "name = " + name + ", id = " + id + ", salary = " + salary;
+	}
+	
 }
+
