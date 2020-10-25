3744de56012fe5d0d7a925f93830f3215d3990ae
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 04:10:07 +0530

    Added:Functionality to write data to a file

--- src/main/java/com/employeePayRoll/EmployeePayrollService.java
+++ src/main/java/com/employeePayRoll/EmployeePayrollService.java
@@ -1,21 +1,30 @@
 package com.employeePayRoll;
-import java.util.*; 
+import java.util.*;
 
 public class EmployeePayrollService {
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	};
 	private List<Employee> employeeList;
-	public EmployeePayrollService() {
-		employeeList = new ArrayList<Employee>(); 
+
+	public EmployeePayrollService(List<Employee> list) {
+		this.employeeList = list;
 	}
 	public static void main(String[] args) {
-		EmployeePayrollService eService = new EmployeePayrollService();
+		ArrayList<Employee> list = new ArrayList<Employee>();
+		EmployeePayrollService eService = new EmployeePayrollService(list);
 		Scanner consoleInput = new Scanner(System.in);
 		eService.readEmployeePayrollData(consoleInput);
-		eService.writeData();
+		eService.writeData(IOService.CONSOLE_IO);
 	}
-	private void writeData() {
-		System.out.println("Writting data of employee to console: "+employeeList);
+	public void writeData(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			System.out.println("Writting data of employee to console: " + employeeList);
+		else if (ioService.equals(IOService.FILE_IO)) {
+			new EmployeeFileService().writeData(employeeList);
+		}
 	}
-	private void readEmployeePayrollData(Scanner consoleInput) {
+	public void readEmployeePayrollData(Scanner consoleInput) {
 		System.out.println("Enter the employee id");
 		int id = consoleInput.nextInt();
 		consoleInput.nextLine();
@@ -23,6 +32,14 @@ public class EmployeePayrollService {
 		String name = consoleInput.nextLine();
 		System.out.println("Enter the employee salary");
 		double salary = consoleInput.nextDouble();
-		employeeList.add(new Employee(id,name,salary));
+		employeeList.add(new Employee(id, name, salary));
+	}
+	public long countEntries(IOService ioService) {
+		long entries = 0;
+		if (ioService.equals(IOService.FILE_IO)) {
+			entries = new EmployeeFileService().countEntries();
+		}
+		return entries;
 	}
 }
+
