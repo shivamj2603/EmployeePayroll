b0b1333a0594d0d14237e17c62eea7aaa4b1479f
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 04:32:14 +0530

    Added:Test case to check whether correct employee entry is printed

--- src/test/java/com/employeePayRoll/EmployeeServiceTest.java
+++ src/test/java/com/employeePayRoll/EmployeeServiceTest.java
@@ -4,9 +4,29 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class EmployeeServiceTest {
-	
 	@Test
 	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
+	@Test
+	//print data
+	public void given3Employee_WhenWrittenToFile_ShouldPrintEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		eService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
+	@Test
+	//count entries
+	public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
 		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
 				new Employee(3, "Mark Zuckerberg", 300000) };
 		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
