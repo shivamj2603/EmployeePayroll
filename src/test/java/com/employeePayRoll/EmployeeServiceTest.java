ae13c5ad52571e465b7f37f7356c413671b1216e
shivamj2603 <shivamj2603@gmail.com>
2020-10-31 04:50:50 +0530

    Added:Test cases to check whether the records are in sync with the database

--- src/test/java/com/employeePayRoll/EmployeeServiceTest.java
+++ src/test/java/com/employeePayRoll/EmployeeServiceTest.java
@@ -6,52 +6,60 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class EmployeeServiceTest {
-//	@Test
-//	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
-//		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
-//				new Employee(3, "Mark Zuckerberg", 300000) };
-//		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-//		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-//		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-//		assertEquals(3, result);
-//	}
-//	@Test
-//	//print data
-//	public void given3Employee_WhenWrittenToFile_ShouldPrintEmployeeEntries() {
-//		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
-//				new Employee(3, "Mark Zuckerberg", 300000) };
-//		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-//		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-//		eService.printData(EmployeePayrollService.IOService.FILE_IO);
-//		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-//		assertEquals(3, result);
-//	}
-//	@Test
-//	//count entries
-//	public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
-//		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
-//				new Employee(3, "Mark Zuckerberg", 300000) };
-//		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-//		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-//		eService.printData(EmployeePayrollService.IOService.FILE_IO);
-//		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-//		assertEquals(3, result);
-//	}
-//	@Test
-//	public void given3Employee_WhenWrittenToFile_ShouldBeReadFromFile() {
-//		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
-//				new Employee(3, "Mark Zuckerberg", 300000) };
-//		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-//		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-//		eService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
-//		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-//		assertEquals(3, result);
-//	}
+	@Test
+	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
+	@Test
+	//print data
+	public void given3Employee_WhenWrittenToFile_ShouldPrintEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		eService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
+	@Test
+	//count entries
+	public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		eService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
+	@Test
+	public void given3Employee_WhenWrittenToFile_ShouldBeReadFromFile() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		eService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
 	@Test
 	public void given3Employee_WhenRetrieved_ShouldMatchEmployeeCount() {
 		List<Employee> employees = new ArrayList<>();
 		EmployeePayrollService eService = new EmployeePayrollService(employees);
 		employees = eService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
 		assertEquals(3, employees.size());
+	}
+	@Test
+	public void givenDatabase_WhenUpdated_ShouldBeInSync() {
+		List<Employee> employees = new ArrayList<>();
+		EmployeePayrollService eService = new EmployeePayrollService(employees);
+		employees = eService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+		eService.updateEmployeeSalary("Terisa", 3000000);
+		eService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
 	}
 }
